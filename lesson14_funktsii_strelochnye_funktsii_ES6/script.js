2c254492fdb15a642e2b07f597090a8f265f9e32
Nupo <example@gmail.com>
2026-08-10 23:51:31 +0500

    Добавил решения задач в lesson14

--- lesson14_funktsii_strelochnye_funktsii_ES6/script.js
+++ lesson14_funktsii_strelochnye_funktsii_ES6/script.js
@@ -11,14 +11,14 @@ showFirstMessage("hello");
 console.log(num); 
 
 
-/* function calc(a, b) {
+function calc(a, b) {
     return (a + b);
 }
 
 console.log(calc(4, 3));
 console.log(calc(4, 6));
 console.log(calc(4, 10));
- */
+
 
 function ret() {
     let num = 50;
@@ -95,7 +95,7 @@ console.log(doNothing() === undefined);
 function sayHello(name) {
     return `Привет, ${name}!`;
 }
-sayHello('Антон');
+console.log(sayHello('Антон'));
 
 
 function returnNeighboringNumbers(number) {
@@ -126,3 +126,172 @@ function getMathResult(num, times) {
 }
 
 console.log(getMathResult(10, 5));
+
+
+
+
+
+/// ЗАДАчки по функциям 
+
+// Задача 1. Приветствие
+function sayHello() {
+    console.log('Привет!');
+}
+sayHello();
+
+
+// Задача 2. Приветствие по имени
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+console.log(sayHello('Антон'));
+console.log(sayHello('Нурболат'));
+
+
+// Задача 3. Сложение
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 3));
+console.log(sum(10, 20));
+
+
+// Задача 4. Умножение
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(5, 4));
+
+
+// Уровень 2. Условия внутри функций
+// Задача 5. Совершеннолетие
+function checkAge(age) {
+    if (age >= 18) {
+        return "Совершеннолетний";
+    } else {
+        return "Не совершеннолетний";
+    }
+}
+console.log(checkAge(22));
+console.log(checkAge(15));
+
+
+// Задача 6. Положительное или отрицательное
+function checkNumber(number) {
+    if (number > 0) {
+        return "Положительное";
+    } else if (number < 0) {
+        return "Отрицательное";
+    } else if (number === 0) {
+        return "Ноль";
+    }
+}
+console.log(checkNumber(10));
+console.log(checkNumber(-5));
+console.log(checkNumber(0));
+
+
+// Задача 7. Чётное или нечётное
+function isEven(number) {
+    if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isEven(10));
+console.log(isEven(7));
+
+
+// Уровень 3. Функции + строки
+// Задача 8. Длина имени
+function getNameLength(name) {
+    return name.length;
+}
+console.log(getNameLength("Антон"));
+
+
+// Задача 9. Большие буквы
+function makeUpper(text) {
+    return text.toUpperCase();
+}
+console.log(makeUpper("javascript"));
+
+
+// Задача 10. Проверка слова
+function hasJavaScript(text) {
+    if (text.includes("JavaScript")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(hasJavaScript("Я изучаю JavaScript"));
+console.log(hasJavaScript("Я изучаю Python"));
+
+
+// Уровень 4. Function Expression и Arrow Function
+// Задача 11. Function Expression
+const subtract = function(a, b) {
+    return a - b;
+}
+console.log(subtract(10, 3));
+
+
+// Задача 12. Arrow Function
+const multiply = (a, b) => {
+    return a * b;
+}
+console.log(multiply(5, 5));
+
+
+// Задача 13. Короткая Arrow Function
+const multiply = (a, b) => a * b;
+console.log(multiply(5, 5));
+
+
+// Уровень 5. Уже надо подумать
+// Задача 14. Максимальное число
+function getMax(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(getMax(10, 5));
+console.log(getMax(3, 8));
+
+
+// Задача 15. Калькулятор
+function calculate(a, b, operation) {
+    if (operation === "+") {
+        return a + b;
+    } else if (operation === "-") {
+        return a - b;
+    } else if (operation === "*") {
+        return a * b;
+    } else if (operation === "/") {
+        return a / b;
+    } else {
+        return "Неизвестная операция";
+    }
+}
+console.log(calculate(10, 5, "+"));
+console.log(calculate(10, 5, 'sfce'));
+
+
+// Задача 16. Самая сложная пока
+function createUserInfo(name, age) {
+    name = name.trim();
+    name = name[0].toUpperCase() + name.slice(1);
+    let status;
+    if (age < 18) {
+        status = "несовершеннолетний";
+    } else {
+        status = "совершеннолетний";
+    }
+    return `Имя: ${name}, возраст: ${age}, статус: ${status}`;
+}
+console.log(createUserInfo("   anton   ", 22));
+console.log(createUserInfo("   alex   ", 16));
