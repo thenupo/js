531958b001f420ba8966aa9fbeab95f62036c1fd
Nupo <example@gmail.com>
2026-08-15 14:31:41 +0500

    Убрал комментарии

--- lesson25_zadachi_s_sobes_na_ponimanye_osnov/script.js
+++ lesson25_zadachi_s_sobes_na_ponimanye_osnov/script.js
@@ -1,44 +1,44 @@
 "use strict";
 
 // 1) Какое будет выведено значение: let x = 5; alert( x++ ); ?
-// let x = 5; alert( x++ );
+let x = 5; alert( x++ );
 
 // 2) Чему равно такое выражение: [ ] + false - null + true ?
-//console.log([ ] + false - null + true)
+console.log([ ] + false - null + true)
 
 // 3) Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
-/* let y = 1; 
+let y = 1; 
 let x = y = 2; 
-alert(x); */
+alert(x); 
 
 // 4) Чему равна сумма [ ] + 1 + 2?
-// console.log([] + 1 + 2);
+console.log([] + 1 + 2);
 
 // 5) Что выведет этот код: alert( "1"[0] );?
-// alert( "1"[0] );
+alert( "1"[0] );
 
 // 6) Чему равно 2 && 1 && null && 0 && undefined ?
-// console.log(2 && 1 && null && 0 && undefined); 
+console.log(2 && 1 && null && 0 && undefined); 
 // И запинается на лжи
 // ИЛИ запинается на правде
 
 // 7) Есть ли разница между выражениями? !!( a && b ) и (a && b)?
-// console.log(!!( 1 && 2 ) === (1 && 2));
+console.log(!!( 1 && 2 ) === (1 && 2));
 
 // 8) Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
-// alert( null || 2 && 3 || 4 );
+alert( null || 2 && 3 || 4 );
 
 // 9) a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
-/* const a = [1, 2, 3];
+const a = [1, 2, 3];
 const b = [1, 2, 3];
-console.log(a == 5); */
+console.log(a == b); 
 
 // 10) Что выведет этот код: alert( +"Infinity" ); ?
-// alert( +"Infinity" );
+alert( +"Infinity" );
 
 // 11) Верно ли сравнение: "Ёжик" > "яблоко"?
-// console.log("Ёжик" > "яблоко");
+console.log("Ёжик" > "яблоко");
 
 // 12) Чему равно 0 || "" || 2 || undefined || true || falsе ?
-// console.log(0 || "" || 2 || undefined || true || falsе); 
+console.log(0 || "" || 2 || undefined || true || falsе); 
 
